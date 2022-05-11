544cc127e3a7cef86a79ec435b8d7261436baca0
edu-as <edu-as@hotmail.com>
2022-05-11 16:54:02 -0300

    feat: handling errors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
-import express from "express";
+import "express-async-errors";
+import express, { NextFunction, Request, Response } from "express";
+import { AppError } from "./errors/AppError";
 import { routes } from "./routes";
 
 const app = express();
@@ -6,5 +8,21 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: "error",
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${err.message}`,
+    });
+  }
+);
 
 app.listen(3003, () => console.log("Server is running on port 3003"));
